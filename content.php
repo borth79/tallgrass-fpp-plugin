b7adec366161adc17656a07006f6a9fd3a9175cf
Brandon <duframe2008@gmail.com>
2020-09-23 12:41:02 -0600

    testing content

--- content.php
+++ content.php
@@ -29,16 +29,11 @@ foreach ($playlists as $playlist) {
 
 $url = "http://127.0.0.1/api/sequence";
 $result = file_get_contents( $url, false, $context );
-$response = json_decode( $result, true );
-print_r($response);
+$sequences = json_decode( $result, true );
 
-foreach ($response as $name) {
-    $url = "http://127.0.0.1/api/sequence/" . $name . "/meta";
-    $result = file_get_contents( $url, false, $context );
-    print_r($result);
-    $responseMeta = json_decode( $result, true );
-    print_r($responseMeta);
-}
+$url = "http://127.0.0.1/api/schedule";
+$result = file_get_contents( $url, false, $context );
+$schedule = json_decode( $result, true );
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -80,6 +75,32 @@ foreach ($response as $name) {
             <small id="fullPlaylistHelp" class="form-text text-muted">Sync your full playlist with TallGrassLights.com</small>
         </div>
 
+        <div class="form-group">
+            <label for="schedule">Full Playlist</label>
+            <select class="form-control" name="schedule" id="schedule" aria-describedby="scheduleHelp">
+                <?php
+                foreach($playlists as $playlist) {
+                    echo '<option value="'.$playlist.'" '. (($store->fullPlaylist === $playlist) ? 'selected' : '') .'>'. $playlist .'</option>';
+                }
+                ?>
+            </select>
+            <small id="scheduleHelp" class="form-text text-muted">Sync your schedule with TallGrassLights.com</small>
+        </div>
+
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+
+<?php
+foreach ($sequences as $name) {
+    $url = "http://127.0.0.1/api/sequence/" . $name . "/meta";
+    $result = file_get_contents( $url, false, $context );
+    print_r($result);
+    $responseMeta = json_decode( $result, true );
+    print_r($responseMeta);
+}
+?>
+
+<?php
+    print_r($schedule);
+?>
 </div>
