8ff3f11ccbf7965b41608f11491cc5b92cebe11c
Brandon <duframe2008@gmail.com>
2020-09-23 10:05:34 -0600

    testing content

--- content.php
+++ content.php
@@ -15,9 +15,15 @@ print_r($response);
 
 $url = "http://127.0.0.1/api/sequence";
 $result = file_get_contents( $url, false, $context );
-print_r($result);
 $response = json_decode( $result, true );
 print_r($response);
+
+foreach ($response as $name) {
+    $url = "http://127.0.0.1/api/sequence/" . $name . "/meta";
+    $result = file_get_contents( $url, false, $context );
+    $responseMeta = json_decode( $result, true );
+    print_r($responseMeta);
+}
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
