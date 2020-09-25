6c817bb2db957a312b3397038f5b57c020d12c41
Brandon <duframe2008@gmail.com>
2020-09-25 00:32:21 -0600

    show_updater

--- show_updater.php
+++ show_updater.php
@@ -10,7 +10,6 @@ function getFppStatus() {
     $response = curl_exec($ch);
     curl_close($ch);
 
-    file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/test4.txt", $response);
     return json_decode($response);
 }
 
@@ -18,14 +17,12 @@ while(true) {
     // get store again in case the the apiKey is updated
     $store = json_decode(file_get_contents($pluginPath . "/store.json"));
 
-    file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/test6.txt", date('H:i:s'));
     $fppStatus = getFppStatus();
     $currentStatus = $fppStatus->status;
     if ($currentStatus !== 1) {
-        file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/test7.txt", 'Show Status:' . json_encode($fppStatus));
+        file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/lastShowStatus.txt", 'Show Status:' . json_encode($fppStatus));
         continue;
     }
-    file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/test5.txt", date('H:i:s'));
     $currentlyPlaying = $fppStatus->current_sequence;
     $fppd = $fppStatus->fppd;
     $scheduler = $fppStatus->scheduler;
