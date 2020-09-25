36e1b16db4dec2f4ac649c534d3009ca88af0390
Brandon <duframe2008@gmail.com>
2020-09-24 22:32:29 -0600

    v1

--- globals.php
+++ globals.php
@@ -60,15 +60,14 @@ function getAllSequences()
 
 function getSequenceData($sequence)
 {
-    file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/test3.txt", "Response:\n" . $sequence);
+    file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/test3.txt", $sequence);
     $url = "http://127.0.0.1/api/sequence/" . str_ireplace(' ', '%20', $sequence) . "/meta";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $response = curl_exec($ch);
     curl_close($ch);
-    file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/responseSequenceDataResponse.txt", "Response:\n" . json_encode($response));
-    file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/test2.txt", "Response:\n" . json_encode($response));
+    file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/responseSequenceDataResponse.txt", json_encode($response));
     return json_decode($response);
 }
 
