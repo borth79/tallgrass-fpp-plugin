642945ada36bc67c37c05904aef851c85b09c208
Brandon <duframe2008@gmail.com>
2020-09-24 23:09:31 -0600

    show_updater

--- show_updater.php
+++ show_updater.php
@@ -19,13 +19,13 @@ while(true) {
     $fppStatus = getFppStatus();
     $currentlyPlaying = $fppStatus->current_sequence;
     file_put_contents("/home/fpp/media/plugins/tallgrass-fpp-plugin/test4.txt", $currentlyPlaying);
-//    $fppd = $fppStatus->fppd;
-//    $scheduler = $fppStatus->scheduler;
-//    $currentlyPlayingStatus = $fppStatus->scheduler->status;
-//    $currentStatus = $fppStatus->status;
-//
-////    $sequecneData = getSequenceData($currentlyPlaying);
-//    #file_put_contents($pluginPath . "/responseTest1.json", json_encode($sequecneData));
+    $fppd = $fppStatus->fppd;
+    $scheduler = $fppStatus->scheduler;
+    $currentlyPlayingStatus = $fppStatus->scheduler->status;
+    $currentStatus = $fppStatus->status;
+
+    $sequecneData = getSequenceData($currentlyPlaying);
+    file_put_contents($pluginPath . "/responseTest1.json", json_encode($sequecneData));
 //
 //    $save = [
 //        'currentlyPlaying' => $currentlyPlaying,
