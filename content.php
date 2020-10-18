6d4c668530177e051a25890bdb47aec8e6d5da12
Brandon <duframe2008@gmail.com>
2020-10-17 23:04:26 -0600

    changed pjlink.php loc

--- content.php
+++ content.php
@@ -39,7 +39,7 @@ $sequences = getAllSequences();
             success: function () {
 
             },
-            dataType: dataType
+            dataType: 'html'
         });
     }
 
@@ -51,7 +51,7 @@ $sequences = getAllSequences();
             success: function () {
 
             },
-            dataType: dataType
+            dataType: 'html'
         });
     }
 
@@ -63,7 +63,7 @@ $sequences = getAllSequences();
             success: function () {
 
             },
-            dataType: dataType
+            dataType: 'html'
         });
     }
 </script>
